c04bf6e3f3d1a266301636d3e7d3234ed9d09067
周元达 <zhouyuanda@duiba.com.cn>
2020-08-26 22:18:09 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from "lodash";
+import "./style.css";
+import Shape from "./shape.png";
+import Data from "./data.xml";
 
 function component() {
   const element = document.createElement("div");
 
   // lodash，现在通过一个 script 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const pic = new Image();
+  pic.src = Shape;
+  element.appendChild(pic);
+
+  console.log(Data);
 
   return element;
 }

a725ac9673edff325c226afdf4c3ab112867cede
周元达 <zhouyuanda@duiba.com.cn>
2020-08-27 16:44:23 +0800

    代码分离

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import _ from "lodash";
-import printMe from "./print";
-import "./style.css";
-
-function component() {
+async function getComponent() {
+  const _ = await import(/* webpackChunkName:'lodash'*/ "lodash");
   const element = document.createElement("div");
-
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  const btn = document.createElement("button");
-
-  btn.innerHTML = "点击查看 log";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }
 
-document.body.appendChild(component());
-
-let element = component();
-document.body.appendChild(element);
-if (module.hot) {
-  module.hot.accept("./print.js", () => {
-    console.log("接收 printMe 模块");
-    document.body.removeChild(element);
-    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
-    document.body.appendChild(element);
-  });
-}
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,19 @@ const isDev = process.env.NODE_ENV === "development";
 module.exports = {
   mode: isDev ? "development" : "production",
   entry: {
-    app: "./src/index.js",
+    index: "./src/index.js",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
   devtool: "cheap-module-eval-source-map",
   devServer: {
     hot: true,
     // publicPath: "/dist/",
     // writeToDisk: true,
-    port: "3000", //默认是8080
+    // port: "3000", //默认是8080
     quiet: false, //默认不启用
     inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
     stats: "errors-only", //终端仅打印 error
@@ -39,6 +44,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.[hash].js",
+    chunkFilename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };

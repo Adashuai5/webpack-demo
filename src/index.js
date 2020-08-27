85766bc04933da8d81e36dc22c780e5ca7974989
周元达 <zhouyuanda@duiba.com.cn>
2020-08-27 10:43:59 +0800

    开发环境

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import printMe from "./print";
+import "./style.css";
 
 function component() {
   const element = document.createElement("div");
@@ -16,3 +17,14 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+let element = component();
+document.body.appendChild(element);
+if (module.hot) {
+  module.hot.accept("./print.js", () => {
+    console.log("接收 printMe 模块");
+    document.body.removeChild(element);
+    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
+    document.body.appendChild(element);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require("path");
 const HtmlWebpackHtml = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const ManifestPlugin = require('webpack-manifest-plugin')
+const ManifestPlugin = require("webpack-manifest-plugin");
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
+  mode: isDev ? "development" : "production",
   entry: {
     app: "./src/index.js",
-    print: "./src/print.js",
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    hot: true,
+    // publicPath: "/dist/",
+    // writeToDisk: true,
+    port: "3000", //默认是8080
+    quiet: false, //默认不启用
+    inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
+    stats: "errors-only", //终端仅打印 error
+    overlay: true, //默认不启用
+    clientLogLevel: "silent", //日志等级
+    compress: true, //是否启用 gzip 压缩
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackHtml({
-      title: "管理输出",
+      title: "开发环境",
     }),
     new CleanWebpackPlugin(),
     new ManifestPlugin(),

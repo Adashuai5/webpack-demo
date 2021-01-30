0ad27ec9115218fe1f7a212d8d0dde702b1ff99c
Adashuai5 <yd.zhou@foxmail.com>
2021-01-30 23:47:46 +0800

    babel

---
+++ my-webpack/let_to_var.ts
@@ -0,0 +1,8 @@
+import { parse } from "@babel/parser";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
+
+const code = `let a = 'let';var b = 2;`;
+const ast = parse(code, { sourceType: "module" });
+
+console.log("ast", ast);

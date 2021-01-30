da50359c573d0a5f46512e5e96382e00d51b85fb
Adashuai5 <yd.zhou@foxmail.com>
2021-01-31 00:02:47 +0800

    let to var

--- my-webpack/let_to_var.ts
+++ my-webpack/let_to_var.ts
@@ -5,4 +5,15 @@ import generate from "@babel/generator";
 const code = `let a = 'let';var b = 2;`;
 const ast = parse(code, { sourceType: "module" });
 
-console.log("ast", ast);
+traverse(ast, {
+  enter: (item) => {
+    if (item.node.type === "VariableDeclaration") {
+      if (item.node.kind === "let") {
+        item.node.kind = "var";
+      }
+    }
+  },
+});
+
+const result = generate(ast, {}, code);
+console.log("code", result);
